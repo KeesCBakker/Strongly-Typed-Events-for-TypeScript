db8619db42e68306db126c12632e23de5279b220
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2016-03-26 23:59:56 +0100

    Added IEventHandling, EventHandlingBase and documentation

--- StronglyTypesEvents-0.1.0.ts
+++ StronglyTypesEvents-0.1.0.ts
@@ -7,6 +7,7 @@
  * Released under the MIT license
  */
 
+/** Models an event with a generic sender and generic arguments */
 interface IEvent<TSender, TArgs> {
 
     subscribe(fn: (sender: TSender, args: TArgs) => void): void;
@@ -14,6 +15,8 @@ interface IEvent<TSender, TArgs> {
     unsubscribe(fn: (sender: TSender, args: TArgs) => void): void;
 }
 
+/** The dispatcher handles the storage of subsciptions and facilitates
+ * subscription, unsubscription and dispatching of the event */
 class EventDispatcher<TSender, TArgs> implements IEvent<TSender, TArgs> {
 
     private _subscriptions: Array<(sender: TSender, args: TArgs) => void> = new Array<(sender: TSender, args: TArgs) => void>();
@@ -38,26 +41,13 @@ class EventDispatcher<TSender, TArgs> implements IEvent<TSender, TArgs> {
     }
 }
 
-class NamedEventDispatch<TSender, TArgs> extends EventDispatcher<TSender, TArgs>
-{
-    private _name: string;
-
-    get name(): string {
-        return this._name;
-    }
-
-    constructor(name: string) {
-        super();
-
-        this._name = name;
-    }
-}
-
+/** Storage class for multiple events that are accessible by name.
+ * Events are automatically created. */
 class EventList<TSender, TArgs> {
 
-    private _events: { [name: string]: NamedEventDispatch<TSender, TArgs>; } = {};
+    private _events: { [name: string]: EventDispatcher<TSender, TArgs>; } = {};
 
-    get(name: string): NamedEventDispatch<TSender, TArgs> {
+    get(name: string): EventDispatcher<TSender, TArgs> {
 
         let event = this._events[name];
 
@@ -65,8 +55,38 @@ class EventList<TSender, TArgs> {
             return event;
         }
 
-        event = new NamedEventDispatch<TSender, TArgs>(name);
+        event = new EventDispatcher<TSender, TArgs>();
         this._events[name] = event;
         return event;
+    }
+
+    remove(name: string): void {
+        this._events[name] = null;
+    }
+}
+
+/** Indicates the object is capable of handling named events. */
+interface IEventHandling<TSender, TArgs> {
+
+    subscribe(name: string, fn: (sender: TSender, args: TArgs) => void): void;
+
+    unsubscribe(name: string, fn: (sender: TSender, args: TArgs) => void): void;
+}
+
+/** Extends objects with event handling capabilities. */
+abstract class EventHandlingBase<TSender, TArgs> implements IEventHandling<TSender, TArgs> {
+
+    private _events: EventList<TSender, TArgs> = new EventList<TSender, TArgs>();
+
+    protected get events(): EventList<TSender, TArgs> {
+        return this._events;
+    }
+
+    subscribe(name: string, fn: (sender: TSender, args: TArgs) => void): void {
+        this._events.get(name).subscribe(fn);
+    }
+
+    unsubscribe(name: string, fn: (sender: TSender, args: TArgs) => void): void {
+        this._events.get(name).unsubscribe(fn);
     }
 }
