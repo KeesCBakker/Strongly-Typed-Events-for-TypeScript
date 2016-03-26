1426dbbecaa69a4e4a9ecab2bbd8791ad2413759
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2016-03-26 22:50:21 +0100

    Update example.pulse-generator.ts

--- example.pulse-generator.ts
+++ example.pulse-generator.ts
@@ -29,37 +29,6 @@ class PulseGenerator {
     }
 }
 
-interface IEvent<TSender, TArgs> {
-
-    subscribe(fn: (sender: TSender, args: TArgs) => void): void;
-
-    unsubscribe(fn: (sender: TSender, args: TArgs) => void): void;
-}
-
-class EventDispatcher<TSender, TArgs> implements IEvent<TSender, TArgs> {
-
-    private _subscriptions: Array<(sender: TSender, args: TArgs) => void> = new Array<(sender: TSender, args: TArgs) => void>();
-
-    subscribe(fn: (sender: TSender, args: TArgs) => void): void {
-        if (fn) {
-            this._subscriptions.push(fn);
-        }
-    }
-
-    unsubscribe(fn: (sender: TSender, args: TArgs) => void): void {
-        let i = this._subscriptions.indexOf(fn);
-        if (i > -1) {
-            this._subscriptions.splice(i, 1);
-        }
-    }
-
-    dispatch(sender: TSender, args: TArgs): void {
-        for (let handler of this._subscriptions) {
-            handler(sender, args);
-        }
-    }
-}
-
 window.onload = function() {
 
   var generator = new PulseGenerator(1);
