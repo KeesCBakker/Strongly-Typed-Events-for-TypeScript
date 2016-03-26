265be71778f8cc22c1b00363bdb8a241e8c521a7
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2016-03-26 21:09:45 +0100

    Added NamedEventDispatcher and EventList
    Also subsituted the vars for lets and introduced a for loop.

--- StronglyTypesEvents-0.1.0.ts
+++ StronglyTypesEvents-0.1.0.ts
@@ -1,5 +1,5 @@
 /*!
- * StronglyTypedEvents for TypeScript
+ * Strongly Typed Events for TypeScript
  * https://github.com/KeesCBakker/StronlyTypedEvents/
  * http://keestalkstech.com
  *
@@ -19,22 +19,54 @@ class EventDispatcher<TSender, TArgs> implements IEvent<TSender, TArgs> {
     private _subscriptions: Array<(sender: TSender, args: TArgs) => void> = new Array<(sender: TSender, args: TArgs) => void>();
 
     subscribe(fn: (sender: TSender, args: TArgs) => void): void {
-        this._subscriptions.push(fn);
+        if (fn) {
+            this._subscriptions.push(fn);
+        }
     }
 
     unsubscribe(fn: (sender: TSender, args: TArgs) => void): void {
-        var i = this._subscriptions.indexOf(fn);
+        let i = this._subscriptions.indexOf(fn);
         if (i > -1) {
             this._subscriptions.splice(i, 1);
         }
     }
 
     dispatch(sender: TSender, args: TArgs): void {
-        for (var i = 0; i < this._subscriptions.length; i++) {
-            var handler = this._subscriptions[i];
-            if (handler != null) {
-                handler(sender, args);
-            }
+        for (let handler of this._subscriptions) {
+            handler(sender, args);
         }
+    }
+}
+
+class NamedEventDispatch<TSender, TArgs> extends EventDispatcher<TSender, TArgs>
+{
+    private _name: string;
+
+    get name(): string {
+        return this._name;
+    }
+
+    constructor(name: string) {
+        super();
+
+        this._name = name;
+    }
+}
+
+class EventList<TSender, TArgs> {
+
+    private _events: { [name: string]: NamedEventDispatch<TSender, TArgs>; } = {};
+
+    get(name: string): NamedEventDispatch<TSender, TArgs> {
+
+        let event = this._events[name];
+
+        if (event) {
+            return event;
+        }
+
+        event = new NamedEventDispatch<TSender, TArgs>(name);
+        this._events[name] = event;
+        return event;
     }
 }
