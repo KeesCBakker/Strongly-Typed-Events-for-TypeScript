00f03d5eec1abb29de6dd818010a78e3c0b700c9
Cornelis Bakker <Cornelis Bakker>
2016-07-09 23:06:00 +0200

    Fixed references

--- examples/example.clock.ts
+++ examples/example.clock.ts
@@ -1,5 +1,5 @@
-﻿/// <reference path="../stronglytypedevents-0.2.0.ts" />
-/// <reference path="../typings/stronglytypedevents-0.2.0.d.ts" />
+﻿/// <reference path="../stronglytypedevents.ts" />
+/// <reference path="../typings/stronglytypedevents.d.ts" />
 
 /*
  * This clock example shows how to use Strongly Typed Events
