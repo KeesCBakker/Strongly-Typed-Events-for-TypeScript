126a29ae0a16012e894f124c3fa3f75edfa1a5a2
Kees C. Bakker <c.bakker@gmx.net>
2018-03-24 10:53:38 +0100

    Removed logging.

--- packages/ste-browser/src/expose.js
+++ packages/ste-browser/src/expose.js
@@ -5,7 +5,6 @@ module.exports = function(data){
     var keys = Object.keys(data);
     for(var i = 0; i < keys.length; i++){
         var name = keys[i];
-        console.log(name);
         w[name] = w[name] || data[name];
         w._e[name] = data[name];
     }
