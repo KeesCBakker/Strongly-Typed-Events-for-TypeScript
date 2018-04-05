e387ebb25113b3d64810ba86057eb4792cf41431
Harold Martinez <me@harold.io>
2018-04-04 23:50:15 -0700

    Use webpack to generate UMD bundles (#20)
    * Use webpack to generate UMD bundles
    
    * No need for externals on strongly-typed-events

---
+++ packages/ste-browser/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+var output = {
+  path: __dirname + '/dist',
+  libraryTarget: 'umd',
+  umdNamedDefine: false
+};
+
+module.exports = [
+  {
+    entry: {
+      'ste-core': './src/ste-core.js',
+      'strongly-typed-events': './src/strongly-typed-events.js'
+    },
+    output: output,
+    devtool: 'source-map'
+  },
+  {
+    entry: {
+      'ste-events': './src/ste-events.js',
+      'ste-signals': './src/ste-signals.js',
+      'ste-simple-events': './src/ste-simple-events.js'
+    },
+    output: output,
+    externals: [
+      'ste-core'
+    ],
+    devtool: 'source-map'
+  }
+];
