90a9789b42e00e1e25de9279574a4dd6b2fdf64a
Kees C. Bakker <c.bakker@gmx.net>
2019-09-24 07:00:46 +0200

    Upgrade packges script. Keeps things up to date.

---
+++ upgrade.ps1
@@ -0,0 +1,51 @@
+echo "Upgrading NCU:"
+npm install -g npm-check-updates
+
+cd packages
+
+echo "Upgrading ste-core:"
+cd ste-core
+ncu -u
+npm install
+cd ..
+
+echo "Upgrading ste-events:"
+cd ste-events
+ncu -u
+npm install
+cd ..
+
+echo "Upgrading ste-signals:"
+cd ste-signals
+ncu -u
+npm install
+cd ..
+
+echo "Upgrading ste-simple-events:"
+cd ste-simple-events
+ncu -u
+npm install
+cd ..
+
+echo "Upgrading strongly-typed-Events:"
+cd strongly-typed-events
+ncu -u
+npm install
+cd ..
+
+echo "Upgrading ste-browser:"
+cd ste-browser
+ncu -u
+npm install
+cd ..
+
+echo "Upgrading root:"
+cd ..
+ncu -u
+npm install
+
+echo "Building & testing:"
+npm build
+npm test
+
+echo "Ready!"
