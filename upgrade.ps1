003c4523d31c8be850ca0d3d245f738dd868770e
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-08-10 07:02:28 +0200

    Packages upgrade

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,49 +1,66 @@
+echo ""
 echo "Upgrading NCU:"
 npm install -g npm-check-updates
+npm audit fix
 
 cd packages
 
+echo ""
 echo "Upgrading ste-core:"
 cd ste-core
 ncu -u
 npm install
+npm audit fix
 cd ..
 
+echo ""
 echo "Upgrading ste-events:"
 cd ste-events
 ncu -u
 npm install
+npm audit fix
 cd ..
 
+echo ""
 echo "Upgrading ste-signals:"
 cd ste-signals
 ncu -u
 npm install
+npm audit fix
 cd ..
 
+echo ""
 echo "Upgrading ste-simple-events:"
 cd ste-simple-events
 ncu -u
 npm install
+npm audit fix
 cd ..
 
+echo ""
 echo "Upgrading strongly-typed-Events:"
 cd strongly-typed-events
 ncu -u
 npm install
+npm audit fix
 cd ..
 
+echo ""
 echo "Upgrading ste-browser:"
 cd ste-browser
 ncu -u
 npm install
+npm audit fix
 cd ..
 
+echo ""
 echo "Upgrading root:"
 cd ..
 ncu -u
 npm install
+npm audit fix
 
+echo ""
 echo "Building & testing:"
 npm run build
 npm test
@@ -51,4 +68,5 @@ npm test
 git add .
 git commit -m "Packages upgrade"
 
+echo ""
 echo "Ready!"
