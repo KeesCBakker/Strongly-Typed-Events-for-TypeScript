ad2bee9c84265d5e976fc15896c036f29e95eac9
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-11-15 21:26:58 +0100

    Added header.

--- packages/ste-events/src/index.ts
+++ packages/ste-events/src/index.ts
@@ -1,3 +1,12 @@
+/*!
+ * Strongly Typed Events for TypeScript - Core
+ * https://github.com/KeesCBakker/StronlyTypedEvents/
+ * http://keestalkstech.com
+ *
+ * Copyright Kees C. Bakker / KeesTalksTech
+ * Released under the MIT license
+ */
+
 import { EventDispatcher } from "./EventDispatcher";
 import { EventHandlingBase } from "./EventHandlingBase";
 import { EventList } from "./EventList";
